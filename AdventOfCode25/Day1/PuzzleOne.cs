15d852e2c1554636b535d3c7aab085f8e82ee878
Tylah Kapa <tylahkapa@gmail.com>
2025-12-03 14:18:50 +1000

    Day 2 Part 2 - Wowee

--- AdventOfCode25/Day1/PuzzleOne.cs
+++ AdventOfCode25/Day1/PuzzleOne.cs
@@ -1,45 +1,84 @@
+using System.Diagnostics;
+
 namespace AdventOfCode25.Day1;
 
 public class PuzzleOne(int value, int maximumValue = 99)
 {
     public int Value { get; private set;  } = value;
+    public int TimesAtZero { get; private set; } = 0;
 
     private readonly char leftSign = 'L';
     private readonly char rightSign = 'R';
     private readonly int _maximumValue = maximumValue;
 
-    public void AddNumber(int number)
-    {
-        var adjustedValue = AdjustedValue(number);
-        var wrappingFix = maximumValue + 1;
-        
-        Value += adjustedValue;
+    private int TotalNumbers => _maximumValue + 1;
 
+    public void AddNumberPart1(int number)
+    {
+        Value += number;
+        
+        Value %= TotalNumbers;
+
+        ClampValue();
+
+        if (Value == 0)
+        {
+            TimesAtZero++;
+        }
+    }
+
+    private bool ClampValue()
+    {
+        var didClamp = false;
+        
         if (Value < 0)
         {
-            Value += wrappingFix;
+            Value += TotalNumbers;
+            didClamp = true;
         }
         else if (Value > _maximumValue)
         {
-            Value -= wrappingFix;
+            Value -= TotalNumbers;
+            didClamp = true;
+        }
+
+        return didClamp;
+    }
+
+    public void AddNumberPart2(int number)
+    {
+        var startingValue = Value;
+        var startedAtZero = Value == 0;
+        Value += (number % TotalNumbers);
+        
+        var wraps = (int)MathF.Round((MathF.Abs(number) / TotalNumbers), 0, MidpointRounding.ToZero);
+        
+        // If the Value == Total Numbers 
+        // Result is Value = 0.
+        // Wraps will also be 1
+
+        var movedThroughZeroOnCorrection = ClampValue() && !startedAtZero && Value != 0;
+        if (movedThroughZeroOnCorrection)
+        {
+            TimesAtZero++;
+        }
+
+        if (Value == 0 && !startedAtZero)
+        {
+            TimesAtZero++;
+        }
+        
+        if (wraps >= 1)
+        {
+            TimesAtZero += wraps;
+        }
+
+        if (Value < 0 || Value > _maximumValue)
+        {
+            Debugger.Break();
         }
     }
-
-    private int AdjustedValue(int number)
-    {
-        var originalSign = MathF.Sign(number);
-        var actualMovementNumber = number % _maximumValue;
-        var timesWrapped = MathF.Abs(number) / _maximumValue;
-
-        if (timesWrapped < 1) return actualMovementNumber;
-        
-        var wraps = (int)MathF.Round(timesWrapped, 0, MidpointRounding.ToZero);
-        if (wraps <= 0) return actualMovementNumber;
-        actualMovementNumber += (wraps * -originalSign);
-
-        return actualMovementNumber;
-    }
-
+    
     public int ParseLine(string line)
     {
         var sign = line[0] == leftSign ? -1 : 1;
--- UnitTests/DayOne/PuzzleOneTests.cs
+++ UnitTests/DayOne/PuzzleOneTests.cs
@@ -26,13 +26,44 @@ public class PuzzleOneTests
     [TestCase(99, -99, 0)]
     [TestCase(0, 14, 14)]
     [TestCase(14, -82, 32)]
-    public void AddNumberShouldReturnCorrectNumber(int startingValue, int addedValue, int finalValue)
+    public void AddNumberPart1ShouldReturnCorrectNumber(int startingValue, int addedValue, int finalValue)
     {
         // Arrange
         var safe = new PuzzleOne(startingValue, _defaultMaximumValue);
         
         // Act
-        safe.AddNumber(addedValue);
+        safe.AddNumberPart1(addedValue);
+        
+        // Assert
+        Assert.That(safe.Value, Is.EqualTo(finalValue));
+    }
+        
+    [TestCase(0, 1, 1)]
+    [TestCase(99, 1, 0)]
+    [TestCase(1, -1, 0)]
+    [TestCase(80, 99, 79)]
+    [TestCase(80, 100, 80)]
+    [TestCase(80, 198, 78)]
+    [TestCase(80, -99, 81)]
+    [TestCase(80, -198, 82)]
+    [TestCase(90, -100, 90)]
+    [TestCase(50, -68, 82)]
+    [TestCase(82, -30, 52)]
+    [TestCase(52, 48, 0)]
+    [TestCase(0, -5, 95)]
+    [TestCase(95, 60, 55)]
+    [TestCase(55, -55, 0)]
+    [TestCase(0, -1, 99)]
+    [TestCase(99, -99, 0)]
+    [TestCase(0, 14, 14)]
+    [TestCase(14, -82, 32)]
+    public void AddNumberPart2ShouldReturnCorrectNumber(int startingValue, int addedValue, int finalValue)
+    {
+        // Arrange
+        var safe = new PuzzleOne(startingValue, _defaultMaximumValue);
+        
+        // Act
+        safe.AddNumberPart2(addedValue);
         
         // Assert
         Assert.That(safe.Value, Is.EqualTo(finalValue));
@@ -61,14 +92,34 @@ public class PuzzleOneTests
     [TestCase(5, "L10", 95)]
     [TestCase(95, "R5", 0)]
     [TestCase(16, "R284", 0)]
-    public void ParsedLineWhenAddedShouldReturnCorrectNumber(int startingValue, string input, int expectedValue)
+    public void ParsedLinePart1WhenAddedShouldReturnCorrectNumber(int startingValue, string input, int expectedValue)
     {
         // Arrange
         var safe = new PuzzleOne(startingValue, _defaultMaximumValue);
         
         // Act
         var addedNumber = safe.ParseLine(input);
-        safe.AddNumber(addedNumber);
+        safe.AddNumberPart1(addedNumber);
+        
+        // Assert
+        Assert.That(safe.Value, Is.EqualTo(expectedValue));
+    }
+    
+    [TestCase(11, "R8", 19)]
+    [TestCase(19, "L19", 0)]
+    [TestCase(0, "L1", 99)]
+    [TestCase(99, "R1", 0)]
+    [TestCase(5, "L10", 95)]
+    [TestCase(95, "R5", 0)]
+    [TestCase(16, "R284", 0)]
+    public void ParsedLinePart2WhenAddedShouldReturnCorrectNumber(int startingValue, string input, int expectedValue)
+    {
+        // Arrange
+        var safe = new PuzzleOne(startingValue, _defaultMaximumValue);
+        
+        // Act
+        var addedNumber = safe.ParseLine(input);
+        safe.AddNumberPart1(addedNumber);
         
         // Assert
         Assert.That(safe.Value, Is.EqualTo(expectedValue));
@@ -76,9 +127,8 @@ public class PuzzleOneTests
 
     [TestCase("Test1.txt", 3)]
     [TestCase("Actual1.txt", 1141)]
-    public void TestVerification(string fileName, int expectedTimes)
+    public void PartOneVerification(string fileName, int expectedTimes)
     {
-        var timesAtZero = 0;
         var startingValue = 50;
         var safe = new PuzzleOne(startingValue, _defaultMaximumValue);
         try
@@ -89,21 +139,8 @@ public class PuzzleOneTests
             // Read the stream as a string.
             foreach (var line in lines)
             {
-                var previous = safe.Value;
                 var addedNumber = safe.ParseLine(line);
-                safe.AddNumber(addedNumber);
-                
-                Console.WriteLine($"Added number: {addedNumber}. Output: {safe.Value}");
-
-                if (safe.Value < 0 || safe.Value > _defaultMaximumValue)
-                {
-                    Console.WriteLine($"Value: {safe.Value}. Expected: {_defaultMaximumValue}");
-                }
-                
-                if (safe.Value == 0)
-                {
-                    timesAtZero++;
-                }
+                safe.AddNumberPart1(addedNumber);
             }
         }
         catch (IOException e)
@@ -112,6 +149,72 @@ public class PuzzleOneTests
             Console.WriteLine(e.Message);
         }
         
-        Assert.That(timesAtZero, Is.EqualTo(expectedTimes));
+        Assert.That(safe.TimesAtZero, Is.EqualTo(expectedTimes));
+    }
+
+    [TestCase(50, -68, 82, 1)]
+    [TestCase(82, -30, 52, 0)]
+    [TestCase(52, 48, 0, 1)]
+    [TestCase(0, -5, 95, 0)]
+    [TestCase(95, 60, 55, 1)]
+    [TestCase(55, -55, 0, 1)]
+    [TestCase(0, -1, 99, 0)]
+    [TestCase(99, -99, 0, 1)]
+    [TestCase(0, 14, 14, 0)]
+    [TestCase(14, -82, 32, 1)]
+    [TestCase(99, -198, 1, 1)]
+    [TestCase(99, -200, 99, 2)]
+    [TestCase(99, 200, 99, 2)]
+    [TestCase(99, 198, 97, 2)]
+    [TestCase(0, 100, 0, 1)]
+    [TestCase(0, 1000, 0, 10)]    
+    [TestCase(0, -100, 0, 1)]
+    [TestCase(0, -1000, 0, 10)]
+    [TestCase(50, -1000, 50, 10)]
+    [TestCase(50, -950, 0, 10)]
+    [TestCase(50, -50, 0, 1)]
+    [TestCase(50, -150, 0, 2)]
+    [TestCase(50, 150, 0, 2)]
+    [TestCase(30, -30, 0, 1)]
+    [TestCase(30, -130, 0, 2)]
+    public void AddNumberPart2ShouldGiveCorrectTimesAtZero(int startingValue, int addedValue, int finalValue,
+        int timesAtZero)
+    {
+        // Arrange
+        var safe = new PuzzleOne(startingValue, _defaultMaximumValue);
+        
+        // Act
+        safe.AddNumberPart2(addedValue);
+        
+        // Assert
+        Assert.That(safe.Value, Is.EqualTo(finalValue));
+        Assert.That(safe.TimesAtZero, Is.EqualTo(timesAtZero));
+    }
+    
+    [TestCase("Test1.txt", 6)]
+    [TestCase("Actual1.txt", 0)]
+    public void PartTwoVerification(string fileName, int expectedTimes)
+    {
+        var startingValue = 50;
+        var safe = new PuzzleOne(startingValue, _defaultMaximumValue);
+        try
+        {
+            // Open the text file using a stream reader.
+            var lines = File.ReadLines($"./DayOne/Inputs/{fileName}");
+
+            // Read the stream as a string.
+            foreach (var line in lines)
+            {
+                var addedNumber = safe.ParseLine(line);
+                safe.AddNumberPart2(addedNumber);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+        
+        Assert.That(safe.TimesAtZero, Is.EqualTo(expectedTimes));
     }
 }
